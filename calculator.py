96e4ed1926b9314e5bd4430d0d6f00b228829d4e
piupiujk <docs4p@yandex.ru>
2024-11-15 18:24:11 +0500

    Программа больше не работает при вводе 0 Добавлены комментарии

--- calculator.py
+++ calculator.py
@@ -1,15 +1,22 @@
 def main(expression: str):
-    digits = [i for i in range(11)]
+    # создание списка чисел
+    digits = [i for i in range(1, 11)]
+    # создание списка знаков
     signs = ['+', '-', '*', '/']
+    # у нас должно быть два числа и знак, поэтому проверяем на количество элементов
     if len(expression.split()) != 3:
         return 'Напишите выражение в виде "2 + 2"'
+    # берем первое число
     first_digit = expression.split()[0]
     try:
+        # проверяем число ли это
         first_digit = int(first_digit)
+        # проверяем положительное ли оно
         if first_digit not in digits:
             return 'Введите положительное число от 1 до 10'
     except:
         return 'Введите целое число'
+    # берем второе число
     second_digit = expression.split()[2]
     try:
         second_digit = int(second_digit)
@@ -17,6 +24,7 @@ def main(expression: str):
             return 'Введите положительное число от 1 до 10'
     except:
         return 'Введите целое число'
+    # берем знак
     current_sign = expression.split()[1]
     if current_sign in signs:
         if current_sign == '+':
@@ -26,6 +34,7 @@ def main(expression: str):
         elif current_sign == '*':
             return first_digit * second_digit
         elif current_sign == '/':
+            # тут перевеодим в целое число по условию задачи
             return int(first_digit / second_digit)
     else:
         return 'Введите одну из арифметических операций "+, -, *, /"'
